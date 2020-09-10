08ce9bbd6d5bb91bdc9a9269fcb5e167b8d3ea79
Silvio <lugaro.iren@gmail.com>
2020-09-10 09:36:09 +0200

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,20 @@
-mkdir -p ~/.streamlit/
-echo "\
+# mkdir -p ~/.streamlit/
+# echo "\
+# [general]\n\
+# email = \"silvio.lugaro@gmail.com\"\n\
+# " > ~/.streamlit/credentials.toml
+# echo "\
+# [server]\n\
+# headless = true\n\
+# enableCORS=false\n\
+# port = $PORT\n\
+
+mkdir -p /root/.streamlit
+bash -c 'echo -e "\
 [general]\n\
-email = \"silvio.lugaro@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-echo "\
+email = \"\"\n\
+" > /root/.streamlit/credentials.toml'
+bash -c 'echo -e "\
 [server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+enableCORS = false\n\
+" > /root/.streamlit/config.toml'
